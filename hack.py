0cbeed0eb3ebf4456a327929107dd35c46b3dd55
daniGaming13114 <68473664+daniGaming13114@users.noreply.github.com>
2020-07-29 12:27:05 +0700

    Add files via upload

---
+++ hack.py
@@ -0,0 +1,163 @@
+import os
+import sys
+import time
+
+print "\33[37;1m==============="
+print "masukkan passwd"
+passwd = raw_input('passwd:\033[1;33m ')
+print "\033[1;31m[+] SALAH...\33[37;1m"
+print "==============="
+if passwd == "dani":
+	os.system('clear')
+	print "\33[32;1mloading...\33[37;1m"
+	time.sleep(1)
+	os.system('pkg install figlet')
+	os.system('pkg install ruby')
+	os.system('gem install lolcat')
+	os.system('pkg install toilet')
+	os.system('pip2 install requests')
+	os.system('clear')
+	os.system('figlet VIRUS HACK | lolcat')
+	print "\033[1;31mV 1.0"
+	time.sleep(1)
+	print "\33[37;1m========================================================="
+	print "\33[36;1mOwner   \33[37;1m:\33[1;33mdaniGaming13114"
+	print "\033[1;31mYou\33[37;1mTube \33[37;1m:\33[1;33mdaniGeming13114"
+	print "\33[30;1mgithub  \33[37;1m:\33[33;1mhttps://github.com/daniGaming13114/"
+	print "\33[37;1m========================================================="
+	print ""
+	time.sleep(1)
+	print "\033[1;32m[+] MENU [+]"
+	print ""
+	print "\33[36;1m[1] 6VIRUS"
+	print "[2] VCRT"
+	print "[3] PENGHANCUR HP"
+	print ""
+	menu = raw_input('\033[1;32m[+] PILIH :\033[1;33m ')
+	if menu == "1":
+		os.system('git clone https://github.com/novalattasya/6VIRUS')
+		print "\33[37;1m==========================="
+		print "\33[32;1m[+] DONE.."
+		time.sleep(1)
+		print "[+] ketik cd 6VIRUS"
+		time.sleep(1)
+		print "[+] lalu ketik sh 6VIRUS.sh"
+		time.sleep(1)
+		print "\33[37;1m==========================="
+		print ""
+	elif menu == "2":
+		os.system('git clone https://github.com/LOoLzeC/Evil-create-framework')
+		print "\33[37;1m=================================="
+		print "\33[32;1m[+] DONE.."
+		time.sleep(1)
+		print "[+] ketik cd Evil-create-framework"
+		time.sleep(1)
+		print "[+] lalu ketik python2 vcrt.py"
+		time.sleep(1)
+		print "\33[37;1m=================================="
+		print ""
+	elif menu == "3":
+		os.system('git clone https://github.com/justahackers/perusak')
+		print "\33[37;1m============================"
+		print "\33[32;1m[+] DONE.."
+		time.sleep(1)
+		print "[+] ketik cd perusak"
+		time.sleep(1)
+		print "[+] ketik python2 perusak.py"
+		time.sleep(1)
+		print "\33[37;1m============================"
+		print ""
+	elif menu == "exit":
+		print "\33[37;1m==========="
+		print "[+] BYE [+]"
+		print "==========="
+		print ""
+	elif menu == "help":
+		print ""
+		print "\33[31;1mketik 1 untuk menjalankan script 6VRUS"
+		print "\33[1;33mketik 2 untuk menjalankan script VCRT"
+		print "\033[1;32mketik 3 untuk menjalankan script PERUSAK HP"
+		print "\33[30;1mketik exit untuk keluar dari scrip"
+		print "\33[37;1m"
+		menu = raw_input('\033[1;32m[+] PILIH :\033[1;33m ')
+			os.system('git clone https://github.com/novalattasya/6VIRUS')                                                         print "\33[37;1m==========================="                                                                          print "\33[32;1m[+] DONE.."
+			print "\33[37;1m==========================="
+                	print "\33[32;1m[+] DONE.."
+	                time.sleep(1)
+        	        print "[+] ketik cd 6VIRUS"
+        	        time.sleep(1)
+	                print "[+] lalu ketik sh 6VIRUS.sh"
+                	time.sleep(1)                                                                                                         print "\33[37;1m==========================="
+                	print ""                                                                                                      elif menu == "2":
+                	os.system('git clone https://github.com/LOoLzeC/Evil-create-framework')                                               print "\33[37;1m=================================="                                                                   print "\33[32;1m[+] DONE.."
+                	time.sleep(1)
+                	print "[+] ketik cd Evil-create-framework"
+                	time.sleep(1)
+                	print "[+] lalu ketik python2 vcrt.py"
+                	time.sleep(1)
+                	print "\33[37;1m=================================="
+                	print ""
+       		 elif menu == "3":
+                	os.system('git clone https://github.com/justahackers/perusak')
+                	print "\33[37;1m============================"
+                	print "\33[32;1m[+] DONE.."
+                	time.sleep(1)
+                	print "[+] ketik cd perusak"
+                	time.sleep(1)
+                	print "[+] ketik python2 perusak.py"
+                	time.sleep(1)
+                	print "\33[37;1m============================"
+                	print ""
+       		 elif menu == "exit":
+                	print "\33[37;1m==========="
+                	print "[+] BYE [+]"
+                	print "==========="
+                	print ""
+       		 elif menu == "help":
+                	print ""
+                	print "\33[31;1mketik 1 untuk menjalankan script 6VRUS"
+                	print "\33[1;33mketik 2 untuk menjalankan script VCRT"
+                	print "\033[1;32mketik 3 untuk menjalankan script PERUSAK HP"
+                	print "\33[30;1mketik exit untuk keluar dari scrip"
+                	print "\33[37;1m"
+                	menu = raw_input('\033[1;32m[+] PILIH :\033[1;33m ')
+				os.system('git clone https://github.com/novalattasya/6VIRUS')                                                         print "\33[37;1m==========================="                                                                          print "\33[32;1m[+] DONE.."
+				print "\33[37;1m==========================="
+                		print "\33[32;1m[+] DONE.."
+			 	time.sleep(1)
+				print "[+] ketik cd 6VIRUS"
+				time.sleep(1)
+				print "[+] lalu ketik sh 6VIRUS.sh"
+				time.sleep(1)                                                                                                         print "\33[37;1m==========================="
+				print ""                                                                                                      elif menu == "2":
+				os.system('git clone https://github.com/LOoLzeC/Evil-create-framework')                                               print "\33[37;1m=================================="                                                                   print "\33[32;1m[+] DONE.."
+                		time.sleep(1)
+                		print "[+] ketik cd Evil-create-framework"
+                		time.sleep(1)
+                		print "[+] lalu ketik python2 vcrt.py"
+                		time.sleep(1)
+                		print "\33[37;1m=================================="
+                		print ""
+       			 elif menu == "3":
+                		os.system('git clone https://github.com/justahackers/perusak')
+                		print "\33[37;1m============================"
+                		print "\33[32;1m[+] DONE.."
+        			time.sleep(1)
+                		print "[+] ketik cd perusak"
+                		time.sleep(1)
+                		print "[+] ketik python2 perusak.py"
+                		time.sleep(1)
+                		print "\33[37;1m============================"
+                		print ""
+       			 elif menu == "exit":
+                		print "\33[37;1m==========="
+                		print "[+] BYE [+]"
+                		print "==========="
+                		print ""
+       			 elif menu == "help":
+                		print ""
+                		print "\33[31;1mketik 1 untuk menjalankan script 6VRUS"
+                		print "\33[1;33mketik 2 untuk menjalankan script VCRT"
+                		print "\033[1;32mketik 3 untuk menjalankan script PERUSAK HP"
+                		print "\33[30;1mketik exit untuk keluar dari scrip"
+        			print "\33[37;1m"
